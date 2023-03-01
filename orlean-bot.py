66007c10e16d4445e5fb63b19393f0c28a7b3f1d
Orlean Lazaro <orleanlazaro@Orleans-MacBook-Air.local>
2023-03-01 14:22:24 -0800

    Adding discord code

--- orlean-bot.py
+++ orlean-bot.py
@@ -1,1 +1,16 @@
-print("Creating discord-bot.")
+import discord 
+from discord.ext import commands 
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents, ssl=False)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Bot is ready!')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run('OTQzNzg0Mzk1MTkyNTYxNzA1.GKOdDf.eotOpoU20x1a0aLhj6zbw9i9hXHLkHKKo6eGQM')
